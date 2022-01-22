680a3706cb6a44feeb75e563afd3aa2f16efdc8e
Apple-03 <j070351@my.cmsh.cyc.edu.tw>
2022-01-22 12:01:06 +0800

    膠輪吸球、撈球

--- src/main/java/frc/robot/groups/SimpleMotor.java
+++ src/main/java/frc/robot/groups/SimpleMotor.java
@@ -1,7 +1,30 @@
 package frc.robot.groups;
 
 public class SimpleMotor {
-    public static void init(){}
+    //When using different functions, please comment out other codes to avoid errors.
+    public static void init(){
+        VictorSP vic = new VictorSP(1);
+    } /*孔位、馬達轉向需再次確認*/
 
-    public static void teleop(){}
+    public static void teleop(){
+        //L型撈球
+        double i = 0.5;//若要改速度改i數值即可，轉向則在if-else結構裡的i加負號
+        if(Robot.xbox.getAButton()){
+            vic.set(i);//往內
+          }else if(xbox.getBButton()){
+            vic.set(-i);//往外
+          }else{
+            vic.set(0);//no press then no move
+          }
+
+        //膠輪吸球
+        double i = 0.5;//若要改速度改i數值即可，轉向則在if-else結構裡的i加負號
+        if(xbox.getAButton()){
+            vic.set(i);//吸球
+        }else if(xbox.getBButton()){
+            vic.set(-i);//以防萬一需反轉吐球
+        }else{
+            vic.set(0);//no press then no move
+        }
+    }
 }
